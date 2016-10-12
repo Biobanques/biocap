6129d3e44203a09c80e4d8bf33ead57ffb695998
mpenicaud <matthieu.penicaud@inserm.fr>
2016-10-12 11:32:35 +0200

    migrate to yii 1.1.17

--- ebiobanques/index-test.php
+++ ebiobanques/index-test.php
@@ -7,7 +7,7 @@
 include dirname(__FILE__) . '/CommonProperties.php';
 include dirname(__FILE__) . '/TestProperties.php';
 //// change the following paths if necessary
-$yii = dirname(__FILE__) . '/yii-1.1.15/framework/yii.php';
+$yii = dirname(__FILE__) . '/yii-1.1.17/framework/yii.php';
 $config = dirname(__FILE__) . '/protected/config/test.php';
 require_once($yii);
 // remove the following line when in production mode
--- ebiobanques/protected/tests/bootstrap.php
+++ ebiobanques/protected/tests/bootstrap.php
@@ -16,7 +16,7 @@ require_once dirname(__FILE__) . '/../../CommonTools.php';
 require_once dirname(__FILE__) . '/../../SmartResearcherTool.php';
 //require_once dirname(__FILE__) . '/../../ExtractContactCommand.php';
 //yii include path
-$yiit = dirname(__FILE__) . '/../../yii-1.1.16/framework/yiit.php';
+$yiit = dirname(__FILE__) . '/../../yii-1.1.17/framework/yiit.php';
 $config = dirname(__FILE__) . '/../config/test.php';
 require_once ($yiit);
 Yii::createWebApplication($config);
